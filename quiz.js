0c68c7c76f8f8101d021c08d885aa11cfec2994f
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-07-14 19:37:59 +0300

    editing on the  quiz.js file

--- quiz.js
+++ quiz.js
@@ -3,23 +3,14 @@ const radioInputs = document.querySelectorAll("input[name='quiz']"),
 submitBtn = document.getElementById("submit-answer"),
 feedback = document.getElementById('feedback');
 
-submitBtn.addEventListener("click" , _ => {
-    // get the checked input----
-    let userAnswer = parseFloat(document.querySelector("input[name='quiz']:checked").value);
-
-    // // loop on radioInputs to get the checked value-----
-    // radioInputs.forEach(input => {
-    //     // check on inputs and assign the value----
-    //     if(input.checked) userAnswer = input.value;
-    // })
-    checkAnswer(userAnswer);
-});
+submitBtn.addEventListener("click" , _ => checkAnswer());
 
 
-function checkAnswer (userAnswer) {
+function checkAnswer () {
     const correctAnswer  = 4;
+    let userAnswer = document.querySelector("input[name='quiz']:checked").value;
     // return feedback---
-    if (userAnswer === correctAnswer) {
+    if (+userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well done.";
     }
     else {
